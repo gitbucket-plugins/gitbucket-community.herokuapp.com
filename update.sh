0efcdf37ae10cef0ec39dae0666426890b0ee7eb
Matthieu Brouillard <matthieu@brouillard.fr>
2017-02-28 13:09:25 +0100

    update to gitbucket 4.10, start plugins update to 4.10
    gist plugin updated to 4.6.0
    announce plugin updated to 1.6.0
    h2 backup plugin updated to 1.4.0
    emoji plugin updated to 4.4.0

--- update.sh
+++ update.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 
-GITBUCKET_VERSION=4.9
+GITBUCKET_VERSION=4.10
 
-GITBUCKET_ANNOUNCE_PLUGIN_VERSION=1.5.0
-GITBUCKET_H2_BACKUP_PLUGIN_VERSION=1.3.0
+GITBUCKET_ANNOUNCE_PLUGIN_VERSION=1.6.0
+GITBUCKET_H2_BACKUP_PLUGIN_VERSION=1.4.0
 GITBUCKET_DESKTOPNOTIFY_PLUGIN_VERSION=4.3.0
 GITBUCKET_COMMITGRAPHS_PLUGIN_VERSION=4.5.0
 GITBUCKET_ASCIIDOCTOR_PLUGIN_VERSION=1.0.1
 GITBUCKET_PAGES_PLUGIN_VERSION=0.7
 GITBUCKET_NETWORK_PLUGIN_VERSION=1.1
-GITBUCKET_EMOJI_PLUGIN_VERSION=4.3.0
+GITBUCKET_EMOJI_PLUGIN_VERSION=4.4.0
 GITBUCKET_RST_PLUGIN_VERSION=0.1.0
 GITBUCKET_BUGSPOTS_PLUGIN_VERSION=4.5.0
-GITBUCKET_GIST_PLUGIN_VERSION=4.5.0
+GITBUCKET_GIST_PLUGIN_VERSION=4.6.0
 
 echo downloading...
 echo -- gitbucket.war.md5 && wget -q -O gitbucket.war.md5 https://github.com/gitbucket/gitbucket/releases/download/$GITBUCKET_VERSION/gitbucket.war.md5 && echo done.
@@ -48,7 +48,7 @@ fi
 if [ ! -f plugins/gitbucket-h2-backup-$GITBUCKET_H2_BACKUP_PLUGIN_VERSION.jar ]
 then
     rm plugins/gitbucket-h2-backup*.jar > /dev/null 2>&1
-    echo -- downloading gitbucket-h2-backup.jar && wget -q -O plugins/gitbucket-h2-backup-$GITBUCKET_H2_BACKUP_PLUGIN_VERSION.jar https://github.com/gitbucket-plugins/gitbucket-h2-backup-plugin/releases/download/$GITBUCKET_H2_BACKUP_PLUGIN_VERSION/gitbucket-h2-backup.jar && echo done.
+    echo -- downloading gitbucket-h2-backup.jar && wget -q -O plugins/gitbucket-h2-backup-$GITBUCKET_H2_BACKUP_PLUGIN_VERSION.jar https://github.com/gitbucket-plugins/gitbucket-h2-backup-plugin/releases/download/$GITBUCKET_H2_BACKUP_PLUGIN_VERSION/gitbucket-h2-backup-plugin.jar && echo done.
 fi
 
 if [ ! -f plugins/gitbucket-desktopnotify-plugin-$GITBUCKET_DESKTOPNOTIFY_PLUGIN_VERSION.jar ]
@@ -84,7 +84,7 @@ fi
 if [ ! -f plugins/gitbucket-emoji-plugin-$GITBUCKET_EMOJI_PLUGIN_VERSION.jar ]
 then
     rm plugins/gitbucket-emoji-plugin*.jar > /dev/null 2>&1
-    echo -- downloading gitbucket-emoji-plugin.jar && wget -q -O plugins/gitbucket-emoji-plugin-$GITBUCKET_EMOJI_PLUGIN_VERSION.jar https://github.com/gitbucket/gitbucket-emoji-plugin/releases/download/$GITBUCKET_EMOJI_PLUGIN_VERSION/gitbucket-emoji-plugin_2.11-$GITBUCKET_EMOJI_PLUGIN_VERSION.jar && echo done.
+    echo -- downloading gitbucket-emoji-plugin.jar && wget -q -O plugins/gitbucket-emoji-plugin-$GITBUCKET_EMOJI_PLUGIN_VERSION.jar https://github.com/gitbucket/gitbucket-emoji-plugin/releases/download/$GITBUCKET_EMOJI_PLUGIN_VERSION/gitbucket-emoji-plugin_2.12-$GITBUCKET_EMOJI_PLUGIN_VERSION.jar && echo done.
 fi
 
 if [ ! -f plugins/gitbucket-rst-plugin-$GITBUCKET_RST_PLUGIN_VERSION.jar ]
@@ -102,7 +102,7 @@ fi
 if [ ! -f plugins/gitbucket-gist-plugin-$GITBUCKET_GIST_PLUGIN_VERSION.jar ]
 then
     rm plugins/gitbucket-gist-plugin*.jar > /dev/null 2>&1
-    echo -- downloading gitbucket-gist-plugin.jar && wget -q -O plugins/gitbucket-gist-plugin-$GITBUCKET_GIST_PLUGIN_VERSION.jar https://github.com/gitbucket/gitbucket-gist-plugin/releases/download/$GITBUCKET_GIST_PLUGIN_VERSION/gitbucket-gist-plugin_2.11-$GITBUCKET_GIST_PLUGIN_VERSION.jar && echo done.
+    echo -- downloading gitbucket-gist-plugin.jar && wget -q -O plugins/gitbucket-gist-plugin-$GITBUCKET_GIST_PLUGIN_VERSION.jar https://github.com/gitbucket/gitbucket-gist-plugin/releases/download/$GITBUCKET_GIST_PLUGIN_VERSION/gitbucket-gist-plugin_2.12-$GITBUCKET_GIST_PLUGIN_VERSION.jar && echo done.
 fi
 
 echo
